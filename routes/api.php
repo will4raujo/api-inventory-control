268109fc730db0f10e12529c6833c72e094f6cc9
willian <willian.araujo1994@gmail.com>
2024-10-04 17:09:03 -0300

    feat: Users CRUD
    - renamed userController to usersController
    - added getById, index and delete

--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,29 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\UsersController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\SuppliersController;
 use App\Http\Controllers\ProductsController;
 
-Route::post('/register', [UserController::class, 'store']);
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::post('/tokens/create', function (Request $request) {
+//     $token = $request->user()->createToken($request->token_name);
+    
+//     return ['token' => $token->plainTextToken];
+// });
+
+//authentification
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
-});
-
+//users
+Route::post('/register', [UsersController::class, 'store']);
+Route::get('/users', [UsersController::class, 'index']);
+Route::get('/users/{id}', [UsersController::class, 'findById']);
+Route::delete('/users/{id}', [UsersController::class, 'destroy']);
 //categories
 Route::get('/categories', [CategoriesController::class, 'index']);
 Route::post('/categories', [CategoriesController::class, 'store']);
