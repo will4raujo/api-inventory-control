482b1a4ec8f54b72993e3f0ffd8fdb89ea96d417
willian <willian.araujo1994@gmail.com>
2024-10-04 14:32:53 -0300

    feat: supplier CRUD
    - added created_at and updated_at in table
    - created supplier model
    - created supplier controller
    - created supplier endpoints

---
+++ app/Http/Controllers/SuppliersController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Supplier;
+
+class SuppliersController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'cnpj' => 'required|string|max:14',
+            'contact' => 'required|string|max:255'
+        ]);
+
+        $supplier = Supplier::create([
+            'name' => $request->name,
+            'cnpj' => $request->cnpj,
+            'contact' => $request->contact
+        ]);
+
+        return response()->json(['message' => 'Supplier created successfully'], 201);
+    }
+
+    public function index()
+    {
+        $suppliers = Supplier::all();
+
+        return response()->json($suppliers);
+    }
+
+    public function findById(Request $request, int $id)
+    {
+        if (!is_numeric($id) || $id <= 0) {
+            return response()->json(['message' => 'Invalid supplier id'], 400);
+        }
+
+        $supplier = Supplier::find($id);
+
+        if (!$supplier) {
+            return response()->json(['message' => 'Supplier not found'], 404);
+        }
+
+        return response()->json($supplier);
+    }
+
+    // public function destroy(int $id)
+    // {
+    //     if (!is_numeric($id) || intval($id) <= 0) {
+    //         return response()->json(['message' => 'Invalid supplier id'], 400);
+    //     }
+
+    //     $supplier = Supplier::find($id);
+    //     if (!$supplier) {
+    //         return response()->json(['message' => 'Supplier not found'], 404);
+    //     }
+
+    //     $products = Product::where('supplier_id', $id)->exists();
+    //     if ($products) {
+    //         return response()->json(['message' => 'Supplier has products and cannot be deleted'], 400);
+    //     }
+
+    //     $supplier->delete();
+
+    //     return response()->json(['message' => 'Supplier deleted successfully']);
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\SuppliersController;
 
 Route::post('/register', [UserController::class, 'store']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -21,9 +22,12 @@ Route::post('/tokens/create', function (Request $request) {
 });
 
 //categories
-
 Route::get('/categories', [CategoriesController::class, 'index']);
 Route::post('/categories', [CategoriesController::class, 'store']);
 Route::get('/categories/{id}', [CategoriesController::class, 'findById']);
 Route::delete('/categories/{id}', [CategoriesController::class, 'destroy']);
-
+//suppliers
+Route::get('/suppliers', [SuppliersController::class, 'index']);
+Route::post('/suppliers', [SuppliersController::class, 'store']);
+Route::get('/suppliers/{id}', [SuppliersController::class, 'findById']);
+Route::delete('/suppliers/{id}', [SuppliersController::class, 'destroy']);
