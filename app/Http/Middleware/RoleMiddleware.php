8146894bc7b1f79a8b720e3ea5c01f62e5716fc4
willian <willian.araujo1994@gmail.com>
2024-10-08 19:34:43 -0300

    fix: roleMiddleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
@@ -12,6 +12,8 @@ class RoleMiddleware
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  string  $role
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next, $role)
     {
@@ -21,7 +23,7 @@ class RoleMiddleware
 
         $user = Auth::user();
 
-        if ($user->role !== $role && $user->role !== 'admin') {
+        if ($user->role_id !== (int) $role && $user->role_id !== 1) { 
             return response()->json(['error' => 'You do not have permission to access this resource'], 403);
         }
 
