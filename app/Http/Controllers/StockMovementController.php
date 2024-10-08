8802e572df465711740ae7221b702f9a0e8b3a24
willian <willian.araujo1994@gmail.com>
2024-10-08 19:34:21 -0300

    fix: filters in StockMovementController

--- app/Http/Controllers/StockMovementController.php
+++ app/Http/Controllers/StockMovementController.php
@@ -8,25 +8,35 @@ use App\Models\Product;
 
 class StockMovementController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $movements = StockMovement::all();
-
+        $movements = StockMovement::query()
+            ->select('stock_movements.*', 'products.name as product_name')
+            ->join('products', 'stock_movements.product_id', '=', 'products.id')
+            ->when($request->name, function ($query) use ($request) {
+                return $query->where('products.name', 'like', '%' . $request->name . '%');
+            })
+            ->get();
+    
         return response()->json($movements);
     }
 
     public function movements(Request $request)
-    {
+    {   
+            $user = auth()->user();
         $movements = StockMovement::query()
-        ->select('stock_movements.type', 'stock_movements.quantity', 'stock_movements.created_at', 'products.name as product_name')
-        ->join('products', 'stock_movements.product_id', '=', 'products.id')
-        ->when($request->type && $request->type !== 'all', function ($query) use ($request) {
-            return $query->where('stock_movements.type', $request->type);
-        })
-        ->orderBy('stock_movements.created_at', 'desc')
-        ->get();
+            ->select('stock_movements.type', 'stock_movements.quantity', 'stock_movements.created_at', 'products.name as product_name')
+            ->join('products', 'stock_movements.product_id', '=', 'products.id')
+            ->when($request->type && $request->type !== 'all', function ($query) use ($request, $user) {
+                if ($request->type === 'my-movements') {
+                    return $query->where('stock_movements.user_id', $user->id);
+                }
+                return $query->where('stock_movements.type', $request->type);
+            })
+            ->orderBy('stock_movements.created_at', 'desc')
+            ->get();
 
-    return response()->json($movements);
+        return response()->json($movements);
     }
 
     public function store(Request $request)
